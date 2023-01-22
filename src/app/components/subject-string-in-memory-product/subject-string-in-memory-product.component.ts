785a48db91a4d04440611f445fa7272a327dae52
Natalia Klekocińska <klekocinskan@gmail.com>
2023-01-22 16:58:24 +0100

    fix products and productsubject in subject-string-in-memory-producy

--- src/app/components/subject-string-in-memory-product/subject-string-in-memory-product.component.ts
+++ src/app/components/subject-string-in-memory-product/subject-string-in-memory-product.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { ProductsModel } from '../../models/products.model';
 import { ProductsService } from '../../services/products.service';
@@ -12,7 +12,8 @@ import { ProductsService } from '../../services/products.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SubjectStringInMemoryProductComponent {
-  readonly products$: Observable<ProductsModel[]> = this._productsService.getAll();
+  private _productSubject: BehaviorSubject<void> = new BehaviorSubject<void>(void 0);
+  readonly products$: Observable<ProductsModel[]> = this._productSubject.asObservable().pipe(switchMap(() => this._productsService.getAll()));
   private _productsDetailsSubject: Subject<number> = new Subject<number>();
   public productsDetails$: Observable<ProductsModel> = this._productsDetailsSubject.asObservable().pipe(
     switchMap(data => this._productsService.getOne(data)));
